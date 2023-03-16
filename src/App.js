d808cdeec13f40e7bbb6007e0eadfafb6e4f518b
Sujeeth <sujeethkrr@gmail.com>
2023-03-16 09:38:14 +0530

    Updated navbar

--- src/App.js
+++ src/App.js
@@ -7,49 +7,11 @@ import Skill from './pages/Skill/Skill';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  // let component;
-  // switch (window.location.pathname) {
-  //   case "/":
-  //     component = <About />
-  //     break;
-  //   case "/about":
-  //     component = <About />
-  //     break;
-  //   case "/skill":
-  //     component = <Skill />
-  //     break;
-  //   case "/project":
-  //     component = <Project />
-  //     break;
-  // }
   return (
     <>
-      <Navbar />
-      {/* <About /> */}
-      <div className="container">
-        <Routes>
-          <Route path='/' element={ <About /> } />
-          <Route path='/about' element={ <About /> }/>
-          <Route path='/resume' element={ <Resume /> }/>
-          <Route path='/skill' element={ <Skill /> }/>
-          <Route path='/project' element={ <Project /> }/>
-        </Routes> 
-        {/* We Use routes in place of switch for reusement */}
-      </div>
+      
     </>
   )
 }
-
-// class App extends React.Component {
-//   render () {
-//     return (
-//       <Typical
-//         steps={['Web developer', 1000, 'Front-end developer', 500]}
-//         loop={Infinity}
-//         wrapper="p"
-//       />
-//     )
-//   }
-// }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';  
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import About from "./pages/About/About";
+import Skill from "./pages/Skill/Skill";
+import Project from "./pages/Project/Project";
+import Resume from "./pages/Resume/Resume";
+import Navbar from "./pages/Navbar/Navbar";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/skill" element={<Skill />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
-
