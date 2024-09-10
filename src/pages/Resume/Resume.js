9f685020c29449682bf93f7ba65ef15e264a6191
Sujeeth <sujeethkrr@gmail.com>
2024-09-10 20:05:35 +0530

    Few changes in the resume page

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -4,7 +4,9 @@ import Resumecard from "./Resumecard";
 import Typical from "react-typical";
 import avasoft from "../../images/avasoft.webp";
 import trustrace from "../../images/trustrace.jpg";
-import './resume1.css';
+import "./resume1.css";
+import { FaAws, FaDocker, FaFigma } from "react-icons/fa";
+import { SiMicrosoftazure } from "react-icons/si";
 
 function Resume() {
   return (
@@ -18,14 +20,19 @@ function Resume() {
             image={avasoft}
             role="Cloud Trainee Engineer"
             date="Feb - Sept 2024"
-            desc="Description"
-            tools="Tools"
+            desc="This experience includes 3 months of training and hands-on work with AWS and Azure services I have hands-on experience with AWS and Azure services, including setting up CI/CD pipelines for application deployment. I’ve also utilized Docker to containerize applications, improving their portability and consistency across environments."
+            tools={[
+              <FaDocker className="resume_icons" />,
+              <FaAws className="resume_icons" />,
+              <SiMicrosoftazure className="resume_icons" />,
+            ]}
           />
           <Resumecard
             image={trustrace}
             role="UI / UX designer"
-            desc="Description"
-            tools="Tools"
+            date="June 2022"
+            desc="As a UI/UX Designer, I have extensive experience in crafting user-centered design solutions for web and mobile applications. My role involved not only the full spectrum of UI/UX design tasks but also a strong focus on developing detailed wireframes to guide the design and development process."
+            tools={[<FaFigma className="resume_icons" />]}
           />
         </div>
       </div>
--- src/pages/Resume/Resumecard.js
+++ src/pages/Resume/Resumecard.js
@@ -1,8 +1,8 @@
 import React from "react";
 import avasoft from "../../images/avasoft.webp";
-import './resume1.css';
+import "./resume1.css";
 
-const Resumecard = ({image, role, desc, tools, date}) => {
+const Resumecard = ({ image, role, desc, tools, date }) => {
   return (
     <div>
       <div className="resume_card-container">
@@ -13,15 +13,16 @@ const Resumecard = ({image, role, desc, tools, date}) => {
           <h3>{role}</h3>
           <h3>{date}</h3>
         </div>
-        <div>
+        <div className="description">
           <p>{desc}</p>
         </div>
         <div>
-          <p>{tools}</p>
+          <p style={{fontWeight: "bolder"}}>Tools: </p>
+          {tools}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Resumecard;
